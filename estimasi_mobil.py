aaea9053d72f24a28e0489e4a4ace1ce93e2baf5
nasywaRaihan <152576928+nasywaRaihan@users.noreply.github.com>
2023-12-01 10:16:52 +0700

    Add files via upload

--- estimasi_mobil.py
+++ estimasi_mobil.py
@@ -6,16 +6,16 @@ model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 st.title('Estimasi Harga Mobil Bekas')
 
 year = st.number_input('Input Tahun Mobil')
-mileage = st.number_input('Input Km Mobil')
+mileage = st.number_input('Input KM Mobil')
 tax = st.number_input('Input Pajak Mobil')
-mpg = st.number_input('Input Konsumsi BBM Mobil')
-enginereSize = st.number_input('Input Engine Size')
+mpg = st.number_input('Input Konsumsi BBm Mobil')
+engineSize = st.number_input('Input Engine Size')
 
 predict = ''
 
 if st.button('Estimasi Harga'):
     predict = model.predict(
-        [[year, mileage, tax, mpg, enginereSize]]
+        [[year, mileage, tax, mpg, engineSize]]
     )
-    st.write ('Estimasi harga mobil bekas dalam Ponds :', predict)
-    st.write ('Estimasi harga mobil bekas dalam IDR (jut) :', predict*19000)
+    st.write ('Estimasi Harga Mobil Bekas Dalam Pounds adalah', predict)
+    st.write ('Estimasi Harga Mobil Bekas Dalam IDR (Juta) adalah', predict*19000)
